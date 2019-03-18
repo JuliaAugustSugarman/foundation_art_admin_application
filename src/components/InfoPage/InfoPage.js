e99548c079c727127d0a57972402d75d3c8ef6b4
JuliaAugustSugarman <jules.august@gmail.com>
2019-03-18 13:54:26 -0500

    teting image sourcing and it works

--- src/components/InfoPage/InfoPage.js
+++ src/components/InfoPage/InfoPage.js
@@ -15,7 +15,7 @@ class infoPage extends Component {
     render() {
         return (
             <div>
-               
+            <img src= "https://media4.s-nbcnews.com/j/newscms/2016_36/1685951/ss-160826-twip-05_8cf6d4cb83758449fd400c7c3d71aa1f.fit-760w.jpg"></img>
           {JSON.stringify(this.props.setArtworkReducer)}
 
             </div>
